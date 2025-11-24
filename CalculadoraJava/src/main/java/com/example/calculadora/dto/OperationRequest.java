f981da2762aa5861a664883faf8c4a9517b38e5e
Jenniiu <jhennygad3333@alu.ufc.br>
2025-11-24 16:52:42 -0300

    feat: calculadoras

---
+++ CalculadoraJava/src/main/java/com/example/calculadora/dto/OperationRequest.java
@@ -0,0 +1,16 @@
+package com.example.calculadora.dto;
+
+public class OperationRequest {
+    private double a;
+    private double b;
+    private String op;
+
+    public double getA() { return a; }
+    public void setA(double a) { this.a = a; }
+
+    public double getB() { return b; }
+    public void setB(double b) { this.b = b; }
+
+    public String getOp() { return op; }
+    public void setOp(String op) { this.op = op; }
+}
